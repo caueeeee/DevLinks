b9ff63a8fbbc88fa8bfc52a646f1b7deb8149f26
souzza <cauelima752@gmail.com>
2025-02-15 20:35:54 -0300

    Basic changes on css

--- script.js
+++ script.js
@@ -4,19 +4,11 @@ function toggleMode() {
 
     const img = document.querySelector('#profile img');
 
-    // Reduz opacidade antes de trocar a imagem
-    img.style.opacity = '0';
-
-    setTimeout(() => {
-        if (html.classList.contains('light')) {
-            img.setAttribute('src', './assets/lightIMG.jfif');
-            img.setAttribute('alt', 'Imagem modo claro');
-        } else {
-            img.setAttribute('src', './assets/Insta @staatues.jfif');
-            img.setAttribute('alt', 'Imagem modo escuro');
-        }
-
-        // Aumenta a opacidade suavemente após trocar a imagem
-        img.style.opacity = '1';
-    }, 150); // Tempo para garantir que a mudança de imagem ocorra suavemente
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/lightIMG.jfif');
+        img.setAttribute('alt', 'Imagem modo claro');
+    } else {
+        img.setAttribute('src', './assets/Insta @staatues.jfif');
+        img.setAttribute('alt', 'Imagem modo escuro');
+    }
 }
