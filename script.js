6b2ebe49f380cdf4e35f8e6e1e716598bcafb708
souzza <cauelima752@gmail.com>
2025-02-15 20:41:15 -0300

    Final adjustes

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function toggleMode() {
     const img = document.querySelector('#profile img');
 
     if (html.classList.contains('light')) {
-        img.setAttribute('src', './assets/lightIMG.jfif');
+        img.setAttribute('src', './assets/avatar-light.png');
         img.setAttribute('alt', 'Imagem modo claro');
     } else {
-        img.setAttribute('src', './assets/Insta @staatues.jfif');
+        img.setAttribute('src', './assets/avatar.png');
         img.setAttribute('alt', 'Imagem modo escuro');
     }
 }
